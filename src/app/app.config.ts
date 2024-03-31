56a46b55dbe4334f27e7fae3fa40d1ec388969b0
Derek Wang <guanghui-wang@foxmail.com>
2024-03-31 15:32:01 +0800

    chore: add provideAnimations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideAnimations(), provideRouter(routes), provideHttpClient()],
 };
