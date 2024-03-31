02e6198d3bbab9a8da546d0ebc861143feef7de4
Derek Wang <guanghui-wang@foxmail.com>
2024-03-31 15:32:40 +0800

    refactor: use standalone module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { StandaloneModule } from '../shared/standalone.module';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [StandaloneModule],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
