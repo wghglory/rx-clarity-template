79b144cfa71cbcd14d591fb4f0cc8dd1a4388a3a
Derek Wang <guanghui-wang@foxmail.com>
2024-06-09 12:42:02 +0800

    feat: refine navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,17 @@
 /* eslint-disable @angular-eslint/prefer-on-push-component-change-detection */
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ClarityIcons, vmBugIcon } from '@cds/core/icon';
+import { angleIcon, ClarityIcons, cogIcon } from '@cds/core/icon';
 import { ClarityModule } from '@clr/angular';
+import { ThemeToggleComponent } from 'clr-lift';
 
-ClarityIcons.addIcons(vmBugIcon);
+ClarityIcons.addIcons(angleIcon, cogIcon);
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ClarityModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ClarityModule, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'rx-clarity-template';
-}
+export class AppComponent {}
